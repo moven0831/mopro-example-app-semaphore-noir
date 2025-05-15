281ec64868474dc45fc1eac14408c1ce0646c660
moven0831 <moven0831@gmail.com>
2025-05-15 09:29:53 +0800

    mopro init

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+// Here we're calling a macro exported with Uniffi. This macro will
+// write some functions and bind them to FFI type. These
+// functions will invoke the `get_circom_wtns_fn` generated below.
+mopro_ffi::app!();
