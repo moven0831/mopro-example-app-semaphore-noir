b9ae97f6213f87e6a4e2865c4d297d916141e286
moven0831 <moven0831@gmail.com>
2025-05-20 16:54:55 +0900

    feat: update APIs for semaphore noir circuit

--- src/lib.rs
+++ src/lib.rs
@@ -14,31 +14,29 @@ use noir::{
 };
 
 #[uniffi::export]
-pub fn prove_keccak256_simple(srs_path: String, inputs: Vec<u8>) -> Vec<u8> {
-    const KECCAK256_JSON: &str = include_str!("../circuit/target/circuit.json");
-    let bytecode_json: serde_json::Value = serde_json::from_str(&KECCAK256_JSON).unwrap();
+pub fn prove_semaphore(srs_path: String, inputs: Vec<String>) -> Vec<u8> {
+    const SEMAPHORE_JSON: &str = include_str!("../circuit/target/circuit.json");
+    let bytecode_json: serde_json::Value = serde_json::from_str(&SEMAPHORE_JSON).unwrap();
     let bytecode = bytecode_json["bytecode"].as_str().unwrap();
 
     setup_srs_from_bytecode(bytecode, Some(&srs_path), false).unwrap();
 
-    let witness_vec_str: Vec<String> = inputs.into_iter().map(|x| x.to_string()).collect();
-    let witness_vec_ref_str: Vec<&str> = witness_vec_str.iter().map(|s| s.as_str()).collect();
+    let witness_vec_ref_str: Vec<&str> = inputs.iter().map(|s| s.as_str()).collect();
 
     let initial_witness = from_vec_str_to_witness_map(witness_vec_ref_str).unwrap();
 
     let start = std::time::Instant::now();
     let proof = prove_ultra_honk(bytecode, initial_witness, false).unwrap();
 
-    println!("Keccak256 proof generation time: {:?}", start.elapsed());
+    println!("Semaphore proof generation time: {:?}", start.elapsed());
 
     proof
 }
 
 #[uniffi::export]
-pub fn verify_keccak256_simple(srs_path: String, proof: Vec<u8>) -> bool {
-    // Assuming the Keccak256 circuit JSON is located at this path
-    const KECCAK256_JSON: &str = include_str!("../circuit/target/circuit.json");
-    let bytecode_json: serde_json::Value = serde_json::from_str(&KECCAK256_JSON).unwrap();
+pub fn verify_semaphore(srs_path: String, proof: Vec<u8>) -> bool {
+    const SEMAPHORE_JSON: &str = include_str!("../circuit/target/circuit.json");
+    let bytecode_json: serde_json::Value = serde_json::from_str(&SEMAPHORE_JSON).unwrap();
     let bytecode = bytecode_json["bytecode"].as_str().unwrap();
 
     setup_srs_from_bytecode(bytecode, Some(&srs_path), false).unwrap();
@@ -48,8 +46,8 @@ pub fn verify_keccak256_simple(srs_path: String, proof: Vec<u8>) -> bool {
     let start = std::time::Instant::now();
     let verdict = verify_ultra_honk(proof, vk).unwrap();
 
-    println!("Keccak256 proof verification time: {:?}", start.elapsed());
-    println!("Keccak256 proof verification verdict: {}", verdict);
+    println!("Semaphore proof verification time: {:?}", start.elapsed());
+    println!("Semaphore proof verification verdict: {}", verdict);
 
     verdict
 }
@@ -63,22 +61,32 @@ mod tests {
 
     #[derive(Deserialize, Debug)]
     struct ProverInput {
-        result: Vec<u8>,
-        x: Vec<u8>,
+        secret_key: String,
+        index_bits: Vec<String>,
+        hash_path: Vec<String>,
+        merkle_proof_length: String,
+        hashed_scope: String,
+        hashed_message: String,
     }
 
     #[test]
-    fn test_prove_and_verify_keccak256_simple() {
-        let srs_path = "test-vectors/noir/zkemail_srs.local".to_string();
+    fn test_prove_and_verify_semaphore() {
+        let srs_path = "test-vectors/noir/semaphore.srs".to_string();
 
         let toml_str = fs::read_to_string("circuit/Prover.toml").expect("Failed to read Prover.toml");
         let prover_input: ProverInput = toml::from_str(&toml_str).expect("Failed to parse Prover.toml");
 
-        let inputs = [prover_input.x, prover_input.result].concat();
+        let mut inputs: Vec<String> = Vec::new();
+        inputs.push(prover_input.secret_key);
+        inputs.extend(prover_input.index_bits);
+        inputs.extend(prover_input.hash_path);
+        inputs.push(prover_input.merkle_proof_length);
+        inputs.push(prover_input.hashed_scope);
+        inputs.push(prover_input.hashed_message);
 
-        let proof = prove_keccak256_simple(srs_path.clone(), inputs);
+        let proof = prove_semaphore(srs_path.clone(), inputs);
         assert!(!proof.is_empty(), "Proof should not be empty");
-        let is_valid = verify_keccak256_simple(srs_path, proof);
+        let is_valid = verify_semaphore(srs_path, proof);
         assert!(is_valid, "Proof verification should succeed");
     }
 }
